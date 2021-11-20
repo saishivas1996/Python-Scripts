ffa0256fa2cd6ad74a4841d48f66f8fa3f753eed
saishivas1996 <saishivas1996@gmail.com>
2021-11-20 11:15:26 +0530

    various types of print statements

--- print_statements.py
+++ print_statements.py
@@ -1,3 +1,24 @@
 print('Hello world')
+#output: Hello world
 
 print('Hello' '\n' 'world')
+
+#output: Hello
+#        world
+
+value1='Hello With variable'
+print(value1)
+
+#output: Hello with variable
+
+value2='1.2.3.4'
+print(value2)
+#output: 1.2.3.4
+
+value3=int(1.2)
+print(value3)
+#output: 1
+
+value4=float(1.234)
+print(value4)
+#output: 1.234
