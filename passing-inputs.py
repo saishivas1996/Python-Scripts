8a2f8df46d57dbb8e48036edeb6b0b2cfaa6f930
saishivas1996 <saishivas1996@gmail.com>
2021-11-20 11:33:37 +0530

    Passing inputs in cli

---
+++ passing-inputs.py
@@ -0,0 +1,11 @@
+value1=input("Enter your Name\n")
+#print("Hello",value1)
+
+value2=int(input("Enter your age\n"))
+print("Hello", value1,"your age is",value2)
+#output: Hello <name> your age is <age>
+
+value4=float(input("Enter your Height\n"))
+print("Hello", value1,"your age is",value2, "and your height is", value4)
+#output: Hello sai your age is 25 and your height is 5.72
+
